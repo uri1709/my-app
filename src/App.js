86b0a141f5b90479d2b6a2dacde5c928b6f8b594
uri1709 <uri1709k@gmail.com>
2024-08-25 16:22:29 +0600

    React deсlaration style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => {
+	const currentYear = new Date().getFullYear(); // Создаем состояние для текущего года
+	return (
+		// здесь использован декларативный стиль написания HTML
+		// императивный стиль HTML  это через createElement
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<p>Текущий год: {currentYear}</p>
+			</header>
+		</div>
+	);
+};
